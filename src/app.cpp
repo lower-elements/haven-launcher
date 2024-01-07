a20d51bf97075d9363c15ddacc1bdf257fbc51ae
Michael Connor Buchan <mikey@blindcomputing.org>
2024-01-07 18:19:20 +0000

    Feat: Enable precompiled headers

--- src/app.cpp
+++ src/app.cpp
@@ -1,4 +1,13 @@
-#include <wx/wx.h>
+// For compilers that support precompilation, includes "wx.h".
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+#   pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#   include <wx/wx.h>
+#endif
 
 #include "app.hpp"
 #include "main_frame.hpp"
--- src/main_frame.cpp
+++ src/main_frame.cpp
@@ -1,4 +1,13 @@
+// For compilers that support precompilation, includes "wx.h".
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
 #include <wx/wx.h>
+#endif
 
 #include "main_frame.hpp"
 
