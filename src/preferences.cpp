472249a52508d94157c790ae9d5c17de70a00138
fintan <blind.lightning.productions@gmail.com>
2024-01-12 07:34:15 +0000

    feat: adds basic general preferences page contents

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -1,6 +1,5 @@
 #include <wx/wx.h>
 #include <wx/preferences.h>
-
 #include <preferences.hpp>
 
 HavenPreferences::HavenPreferences(const wxString &title) : wxPreferencesEditor(title) {
@@ -20,7 +19,35 @@ wxString HavenPreferencesGeneral::GetName() const {
     return "general";
 };
 wxWindow* HavenPreferencesGeneral::CreateWindow(wxWindow *parent) {
-    return new wxPanel(parent);
+    wxPanel* Panel = new wxPanel(parent);
+    wxFlexGridSizer* Sizer = new wxFlexGridSizer(2, 5, 5);
+
+    //The language picker
+    wxChoice* LanguageChoice = new wxChoice(Panel, wxID_ANY);
+    LanguageChoice->SetExtraStyle(wxCB_SORT);
+    LanguageChoice->SetName("Language");
+    LanguageChoice->Append("English");
+    LanguageChoice->SetSelection(0);
+
+    Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Language"));
+    Sizer->Add(LanguageChoice);
+
+    wxCheckBox* LauncherUpdates = new wxCheckBox(Panel, wxID_ANY, "Check for updates to the launcher on opening the app");
+    LauncherUpdates->SetValue(1);
+    Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
+    Sizer->Add(LauncherUpdates);
+    wxCheckBox* GameUpdates = new wxCheckBox(Panel, wxID_ANY, "Check for updates to Installed Games on the opening of the app");
+    GameUpdates->SetValue(1);
+    Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
+    Sizer->Add(GameUpdates);
+
+
+    Panel->SetSizer(Sizer);
+    Panel->Layout();
+
+    return Panel;
+
+
 }
 
 HavenPreferencesRepository::HavenPreferencesRepository() : wxPreferencesPage() {};
