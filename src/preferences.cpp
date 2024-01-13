5a33f886e52753ae47ed5577c39f77b0639332cd
Michael Connor Buchan <mikey@blindcomputing.org>
2024-01-13 14:37:20 +0000

    feat: Use precompiled headers in preferences.cpp

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -1,6 +1,16 @@
-#include <wx/preferences.h>
+// For compilers that support precompilation, includes "wx.h".
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
 #include <wx/wx.h>
+#endif
+#include <wx/preferences.h>
 #include <wx/spinctrl.h>
+
 #include <preferences.hpp>
 
 HavenPreferences::HavenPreferences(const wxString &title)
@@ -12,45 +22,43 @@ HavenPreferences::HavenPreferences(const wxString &title)
 
 HavenPreferences::~HavenPreferences(){};
 
+HavenPreferencesGeneral::HavenPreferencesGeneral() : wxPreferencesPage(){};
+HavenPreferencesGeneral::~HavenPreferencesGeneral(){};
+wxString HavenPreferencesGeneral::GetName() const { return "general"; };
+wxWindow *HavenPreferencesGeneral::CreateWindow(wxWindow *parent) {
+  wxPanel *Panel = new wxPanel(parent);
+  wxFlexGridSizer *Sizer = new wxFlexGridSizer(2, 5, 5);
 
+  // The language picker
+  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Language"));
+  wxChoice *LanguageChoice = new wxChoice(Panel, wxID_ANY);
+  LanguageChoice->SetExtraStyle(wxCB_SORT);
+  LanguageChoice->SetName("Language");
+  LanguageChoice->Append("English");
+  LanguageChoice->SetSelection(0);
 
-HavenPreferencesGeneral::HavenPreferencesGeneral() : wxPreferencesPage() {};
-HavenPreferencesGeneral::~HavenPreferencesGeneral() {};
-wxString HavenPreferencesGeneral::GetName() const {
-    return "general";
-};
-wxWindow* HavenPreferencesGeneral::CreateWindow(wxWindow *parent) {
-    wxPanel* Panel = new wxPanel(parent);
-    wxFlexGridSizer* Sizer = new wxFlexGridSizer(2, 5, 5);
+  Sizer->Add(LanguageChoice);
 
-    //The language picker
-    Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Language"));
-    wxChoice* LanguageChoice = new wxChoice(Panel, wxID_ANY);
-    LanguageChoice->SetExtraStyle(wxCB_SORT);
-    LanguageChoice->SetName("Language");
-    LanguageChoice->Append("English");
-    LanguageChoice->SetSelection(0);
+  Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
+  wxCheckBox *LauncherUpdates = new wxCheckBox(
+      Panel, wxID_ANY, "Check for updates to the launcher on opening the app");
+  LauncherUpdates->SetValue(1);
+  Sizer->Add(LauncherUpdates);
+  Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
+  wxCheckBox *GameUpdates = new wxCheckBox(
+      Panel, wxID_ANY,
+      "Check for updates to Installed Games on the opening of the app");
+  GameUpdates->SetValue(1);
+  Sizer->Add(GameUpdates);
 
-    Sizer->Add(LanguageChoice);
+  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Installation Directory"));
+  wxTextCtrl *InstallDir = new wxTextCtrl(Panel, wxID_ANY, "./library/games/");
+  Sizer->Add(InstallDir);
 
-    Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
-    wxCheckBox* LauncherUpdates = new wxCheckBox(Panel, wxID_ANY, "Check for updates to the launcher on opening the app");
-    LauncherUpdates->SetValue(1);
-    Sizer->Add(LauncherUpdates);
-    Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
-    wxCheckBox* GameUpdates = new wxCheckBox(Panel, wxID_ANY, "Check for updates to Installed Games on the opening of the app");
-    GameUpdates->SetValue(1);
-    Sizer->Add(GameUpdates);
+  Panel->SetSizer(Sizer);
+  Panel->Layout();
 
-    Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Installation Directory"));
-    wxTextCtrl* InstallDir = new wxTextCtrl(Panel, wxID_ANY, "./library/games/");
-    Sizer->Add(InstallDir);
-
-
-    Panel->SetSizer(Sizer);
-    Panel->Layout();
-
-    return Panel;
+  return Panel;
 }
 
 HavenPreferencesRepository::HavenPreferencesRepository()
@@ -60,17 +68,18 @@ wxString HavenPreferencesRepository::GetName() const {
   return "repository settings";
 };
 wxWindow *HavenPreferencesRepository::CreateWindow(wxWindow *parent) {
-  wxPanel* Panel = new wxPanel(parent);
-  wxFlexGridSizer* Sizer = new wxFlexGridSizer(4, 5, 5);
+  wxPanel *Panel = new wxPanel(parent);
+  wxFlexGridSizer *Sizer = new wxFlexGridSizer(4, 5, 5);
   Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Repositories"));
-  wxChoice* RepoList = new wxChoice(Panel, wxID_ANY);
+  wxChoice *RepoList = new wxChoice(Panel, wxID_ANY);
 
   Sizer->Add(RepoList);
   Sizer->Add(new wxButton(Panel, wxID_ANY, "Add"));
   Sizer->Add(new wxButton(Panel, wxID_ANY, "Edit"));
   Sizer->Add(new wxButton(Panel, wxID_ANY, "remove"));
 
-  wxCheckBox* TrustedRepos = new wxCheckBox(Panel, wxID_ANY, "Always load trusted repos");
+  wxCheckBox *TrustedRepos =
+      new wxCheckBox(Panel, wxID_ANY, "Always load trusted repos");
   TrustedRepos->SetValue(1);
   Sizer->Add(TrustedRepos, wxEXPAND);
   Panel->SetSizer(Sizer);
@@ -84,23 +93,22 @@ wxString HavenPreferencesDownload::GetName() const {
   return "download settings";
 };
 wxWindow *HavenPreferencesDownload::CreateWindow(wxWindow *parent) {
-  wxPanel* Panel = new wxPanel(parent);
-  wxFlexGridSizer* Sizer = new wxFlexGridSizer(2, 5, 5);
+  wxPanel *Panel = new wxPanel(parent);
+  wxFlexGridSizer *Sizer = new wxFlexGridSizer(2, 5, 5);
   Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Download Directory"));
-  wxTextCtrl* DownloadDir = new wxTextCtrl(Panel, wxID_ANY, "./library/downloads/");
+  wxTextCtrl *DownloadDir =
+      new wxTextCtrl(Panel, wxID_ANY, "./library/downloads/");
   Sizer->Add(DownloadDir);
 
   Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Download throttle (mb/s)"));
-  wxSpinCtrl* DownloadthrottleMB = new wxSpinCtrl(Panel, wxID_ANY, "000");
-  DownloadthrottleMB->SetRange(0,100);
+  wxSpinCtrl *DownloadthrottleMB = new wxSpinCtrl(Panel, wxID_ANY, "000");
+  DownloadthrottleMB->SetRange(0, 100);
   Sizer->Add(DownloadthrottleMB);
   Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Download throttle (kb/s)"));
-  wxSpinCtrl* DownloadthrottleKB = new wxSpinCtrl(Panel, wxID_ANY, "0000");
-  DownloadthrottleKB->SetRange(0,1023);
+  wxSpinCtrl *DownloadthrottleKB = new wxSpinCtrl(Panel, wxID_ANY, "0000");
+  DownloadthrottleKB->SetRange(0, 1023);
   DownloadthrottleKB->SetBase(2);
   Sizer->Add(DownloadthrottleKB);
-
-
 
   Panel->SetSizer(Sizer);
   Panel->Layout();
