90459525e3b5a7a00802d339c7408c3d77ff738d
fintan <blind.lightning.productions@gmail.com>
2024-01-15 17:47:16 +0000

    feat: adds functionality to saving and reading config data, place holder values for now

--- include/preferences.hpp
+++ include/preferences.hpp
@@ -2,52 +2,113 @@
 #define HAVEN_PREFERENCES_HPP
 
 #include <wx/preferences.h>
+#include <wx/config.h>
+#include <wx/wx.h>
+
 
 class HavenPreferencesGeneral;
+class GeneralPanel;
+
 class HavenPreferencesRepository;
+class RepositoryPanel;
+
 class HavenPreferencesDownload;
+class DownloadPanel;
 
 
 class HavenPreferences : public wxPreferencesEditor {
 public:
-  HavenPreferences(const wxString &title);
+  HavenPreferences(const wxString &title, wxConfig& Config);
   virtual ~HavenPreferences() override;
 
 private:
-  // event definitions
-
-
-  // event table
+  wxConfig& M_Config;
 };
 
-// event IDs go here
 
 
 class HavenPreferencesGeneral : public wxPreferencesPage {
 public:
-  HavenPreferencesGeneral();
+  HavenPreferencesGeneral(wxConfig& Config);
   virtual ~HavenPreferencesGeneral() override;
 
   virtual wxString GetName() const override;
   virtual wxWindow* CreateWindow(wxWindow *parent) override;
+  
+private:
+  wxConfig& M_Config;
 };
 
 class HavenPreferencesRepository : public wxPreferencesPage {
 public:
-  HavenPreferencesRepository();
+  HavenPreferencesRepository(wxConfig& Config);
   virtual ~HavenPreferencesRepository();
 
   virtual wxString GetName() const override;
   virtual wxWindow* CreateWindow(wxWindow *parent) override;
+
+private:
+  wxConfig& M_Config;
 };
 
 class HavenPreferencesDownload : public wxPreferencesPage {
 public:
-  HavenPreferencesDownload();
+  HavenPreferencesDownload(wxConfig& Config);
   virtual ~HavenPreferencesDownload();
 
   virtual wxString GetName() const override;
   virtual wxWindow* CreateWindow(wxWindow *parent) override;
+
+private:
+  wxConfig& M_Config;
+};
+
+
+
+class GeneralPanel : public wxPanel {
+public:
+  GeneralPanel(wxWindow* Parent, wxConfig& Config);
+  void OnLang(wxCommandEvent &event);
+  void OnGameUpdates(wxCommandEvent &event);
+  void OnLauncherUpdates(wxCommandEvent &event);
+  void OnGameDir(wxCommandEvent &event);
+private:
+  wxConfig& M_Config;
+
+  wxDECLARE_EVENT_TABLE();
+};
+
+class RepositoryPanel : public wxPanel {
+public:
+  RepositoryPanel(wxWindow* Parent, wxConfig& Config);
+private:
+  wxConfig& M_Config;
+
+  wxDECLARE_EVENT_TABLE();
+};
+
+class DownloadPanel : public wxPanel {
+public:
+  DownloadPanel(wxWindow* Parent, wxConfig& Config);
+private:
+  wxConfig& M_Config;
+
+  wxDECLARE_EVENT_TABLE();
+};
+
+
+
+
+enum {
+  ID_LANG,
+  ID_LAUNCHER_UPDATES,
+  ID_GAME_UPDATES,
+  ID_GAMES_DIR_EDIT,
+  ID_GAMES_DIR_BROWSE,
+  ID_DOWNLOAD_DIR__EDIT,
+  ID_DOWNLOAD_DIR_BROWSE,
+  ID_DOWNLOAD_THROTTLE_MB,
+  ID_DOWNLOAD_THROTTLE_KB
 };
 
 #endif
--- src/preferences.cpp
+++ src/preferences.cpp
@@ -1,5 +1,6 @@
 // For compilers that support precompilation, includes "wx.h".
 #include <wx/wxprec.h>
+#include <limits>
 
 #ifdef __BORLANDC__
 #pragma hdrstop
@@ -9,108 +10,165 @@
 #include <wx/wx.h>
 #endif
 #include <wx/preferences.h>
+#include <wx/config.h>
 #include <wx/spinctrl.h>
 
 #include <preferences.hpp>
 
-HavenPreferences::HavenPreferences(const wxString &title)
-    : wxPreferencesEditor(title) {
-  AddPage(new HavenPreferencesGeneral());
-  AddPage(new HavenPreferencesRepository());
-  AddPage(new HavenPreferencesDownload());
+HavenPreferences::HavenPreferences(const wxString &title, wxConfig& Config)
+    : wxPreferencesEditor(title), M_Config(Config) {
+  AddPage(new HavenPreferencesGeneral(M_Config));
+  AddPage(new HavenPreferencesRepository(M_Config));
+  AddPage(new HavenPreferencesDownload(M_Config));
 };
 
 HavenPreferences::~HavenPreferences(){};
 
-HavenPreferencesGeneral::HavenPreferencesGeneral() : wxPreferencesPage(){};
+HavenPreferencesGeneral::HavenPreferencesGeneral(wxConfig& Config) : wxPreferencesPage(), M_Config(Config) {};
 HavenPreferencesGeneral::~HavenPreferencesGeneral(){};
 wxString HavenPreferencesGeneral::GetName() const { return "general"; };
 wxWindow *HavenPreferencesGeneral::CreateWindow(wxWindow *parent) {
-  wxPanel *Panel = new wxPanel(parent);
-  wxFlexGridSizer *Sizer = new wxFlexGridSizer(2, 5, 5);
-
-  // The language picker
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Language"));
-  wxChoice *LanguageChoice = new wxChoice(Panel, wxID_ANY);
-  LanguageChoice->SetExtraStyle(wxCB_SORT);
-  LanguageChoice->SetName("Language");
-  LanguageChoice->Append("English");
-  LanguageChoice->SetSelection(0);
-
-  Sizer->Add(LanguageChoice);
-
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
-  wxCheckBox *LauncherUpdates = new wxCheckBox(
-      Panel, wxID_ANY, "Check for updates to the launcher on opening the app");
-  LauncherUpdates->SetValue(1);
-  Sizer->Add(LauncherUpdates);
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, ""));
-  wxCheckBox *GameUpdates = new wxCheckBox(
-      Panel, wxID_ANY,
-      "Check for updates to Installed Games on the opening of the app");
-  GameUpdates->SetValue(1);
-  Sizer->Add(GameUpdates);
-
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Installation Directory"));
-  wxTextCtrl *InstallDir = new wxTextCtrl(Panel, wxID_ANY, "./library/games/");
-  Sizer->Add(InstallDir);
-
-  Panel->SetSizer(Sizer);
-  Panel->Layout();
-
-  return Panel;
+  return new GeneralPanel(parent, M_Config);
 }
 
-HavenPreferencesRepository::HavenPreferencesRepository()
-    : wxPreferencesPage(){};
+HavenPreferencesRepository::HavenPreferencesRepository(wxConfig& Config)
+    : wxPreferencesPage(), M_Config(Config) {};
 HavenPreferencesRepository::~HavenPreferencesRepository(){};
 wxString HavenPreferencesRepository::GetName() const {
   return "repository settings";
 };
 wxWindow *HavenPreferencesRepository::CreateWindow(wxWindow *parent) {
-  wxPanel *Panel = new wxPanel(parent);
-  wxFlexGridSizer *Sizer = new wxFlexGridSizer(4, 5, 5);
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Repositories"));
-  wxChoice *RepoList = new wxChoice(Panel, wxID_ANY);
-
-  Sizer->Add(RepoList);
-  Sizer->Add(new wxButton(Panel, wxID_ANY, "Add"));
-  Sizer->Add(new wxButton(Panel, wxID_ANY, "Edit"));
-  Sizer->Add(new wxButton(Panel, wxID_ANY, "remove"));
-
-  wxCheckBox *TrustedRepos =
-      new wxCheckBox(Panel, wxID_ANY, "Always load trusted repos");
-  TrustedRepos->SetValue(1);
-  Sizer->Add(TrustedRepos, wxEXPAND);
-  Panel->SetSizer(Sizer);
-  Panel->Layout();
-  return Panel;
+  return new RepositoryPanel(parent, M_Config);
 };
 
-HavenPreferencesDownload::HavenPreferencesDownload() : wxPreferencesPage(){};
+HavenPreferencesDownload::HavenPreferencesDownload(wxConfig& Config) : wxPreferencesPage(), M_Config(Config) {};
 HavenPreferencesDownload::~HavenPreferencesDownload(){};
 wxString HavenPreferencesDownload::GetName() const {
   return "download settings";
 };
 wxWindow *HavenPreferencesDownload::CreateWindow(wxWindow *parent) {
-  wxPanel *Panel = new wxPanel(parent);
-  wxFlexGridSizer *Sizer = new wxFlexGridSizer(2, 5, 5);
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Download Directory"));
-  wxTextCtrl *DownloadDir =
-      new wxTextCtrl(Panel, wxID_ANY, "./library/downloads/");
-  Sizer->Add(DownloadDir);
+  return new DownloadPanel(parent, M_Config);
+};
 
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Download throttle (mb/s)"));
-  wxSpinCtrl *DownloadthrottleMB = new wxSpinCtrl(Panel, wxID_ANY, "000");
-  DownloadthrottleMB->SetRange(0, 100);
+
+GeneralPanel::GeneralPanel(wxWindow* Parent, wxConfig& Config) : wxPanel(Parent), M_Config(Config) {
+  wxFlexGridSizer *Sizer = new wxFlexGridSizer(2, 5, 5);
+
+  // The language picker
+  Sizer->Add(new wxStaticText(this, wxID_ANY, "Language"));
+  wxChoice *LanguageChoice = new wxChoice(this, ID_LANG);
+  LanguageChoice->SetExtraStyle(wxCB_SORT);
+  LanguageChoice->SetName("Language");
+  LanguageChoice->Append("English");
+  LanguageChoice->Append("Polish");
+  LanguageChoice->Append("Japanese");
+  wxString data = M_Config.Read("preferences/general/language", "English");
+  int ChoiceIndex = LanguageChoice->FindString(data);
+  LanguageChoice->SetSelection(ChoiceIndex);
+
+  Sizer->Add(LanguageChoice);
+
+  Sizer->Add(new wxStaticText(this, wxID_ANY, ""));
+  wxCheckBox *LauncherUpdates = new wxCheckBox(
+      this, ID_LAUNCHER_UPDATES, "Check for updates to the launcher on opening the app");
+  LauncherUpdates->SetValue(M_Config.Read("preferences/general/launcher_updates", 1));
+  Sizer->Add(LauncherUpdates);
+  Sizer->Add(new wxStaticText(this, wxID_ANY, ""));
+  wxCheckBox *GameUpdates = new wxCheckBox(
+      this, ID_GAME_UPDATES,
+      "Check for updates to Installed Games on the opening of the app");
+  GameUpdates->SetValue(M_Config.Read("preferences/general/game_updates", 1));
+  Sizer->Add(GameUpdates);
+
+  Sizer->Add(new wxStaticText(this, wxID_ANY, "Installation Directory"));
+  wxTextCtrl *InstallDir = new wxTextCtrl(this, ID_GAMES_DIR_EDIT, M_Config.Read("preferences/general/games_dir", "./library/games/"));
+  Sizer->Add(InstallDir);
+
+  SetSizer(Sizer);
+  Layout();
+
+}
+
+void GeneralPanel::OnLang(wxCommandEvent &event)
+{
+  M_Config.Write("preferences/general/language", event.GetString());
+}
+
+void GeneralPanel::OnGameUpdates(wxCommandEvent &event)
+{
+  M_Config.Write("preferences/general/game_updates", event.GetInt());
+}
+
+void GeneralPanel::OnLauncherUpdates(wxCommandEvent &event)
+{
+  M_Config.Write("preferences/general/launcher_updates", event.GetInt());
+}
+
+void GeneralPanel::OnGameDir(wxCommandEvent &event)
+{
+  M_Config.Write("preferences/general/games_dir", event.GetString());
+}
+
+RepositoryPanel::RepositoryPanel(wxWindow* parent, wxConfig& Config) 
+  : wxPanel(parent), M_Config(Config) 
+{
+  wxFlexGridSizer *Sizer = new wxFlexGridSizer(4, 5, 5);
+  Sizer->Add(new wxStaticText(this, wxID_ANY, "Repositories"));
+  wxChoice *RepoList = new wxChoice(this, wxID_ANY);
+
+  Sizer->Add(RepoList);
+  Sizer->Add(new wxButton(this, wxID_ANY, "Add"));
+  Sizer->Add(new wxButton(this, wxID_ANY, "Edit"));
+  Sizer->Add(new wxButton(this, wxID_ANY, "remove"));
+
+  wxCheckBox *TrustedRepos = new wxCheckBox(this, wxID_ANY, "Always load trusted repos");
+  TrustedRepos->SetValue(1);
+  Sizer->Add(TrustedRepos, wxEXPAND);
+  SetSizer(Sizer);
+  Layout();
+}
+
+DownloadPanel::DownloadPanel(wxWindow* parent, wxConfig& Config) 
+  : wxPanel(parent), M_Config(Config)
+{
+  wxFlexGridSizer *Sizer = new wxFlexGridSizer(2, 5, 5);
+  Sizer->Add(new wxStaticText(this, wxID_ANY, "Download Directory"));
+  wxTextCtrl *DownloadDir =
+      new wxTextCtrl(this, wxID_ANY, "./library/downloads/");
+  Sizer->Add(DownloadDir);
+  Sizer->Add(new wxStaticText(this, wxID_ANY, "select a directory"));
+  Sizer->Add(new wxButton(this, wxID_ANY, "Browse"));
+
+  Sizer->Add(new wxStaticText(this, wxID_ANY, "Download throttle (mb/s)"));
+  wxSpinCtrl *DownloadthrottleMB = new wxSpinCtrl(this, wxID_ANY, "0");
+  DownloadthrottleMB->SetRange(0, std::numeric_limits<int>::max());
   Sizer->Add(DownloadthrottleMB);
-  Sizer->Add(new wxStaticText(Panel, wxID_ANY, "Download throttle (kb/s)"));
-  wxSpinCtrl *DownloadthrottleKB = new wxSpinCtrl(Panel, wxID_ANY, "0000");
+  Sizer->Add(new wxStaticText(this, wxID_ANY, "Download throttle (kb/s)"));
+  wxSpinCtrl *DownloadthrottleKB = new wxSpinCtrl(this, wxID_ANY, "0");
   DownloadthrottleKB->SetRange(0, 1023);
   DownloadthrottleKB->SetBase(2);
   Sizer->Add(DownloadthrottleKB);
 
-  Panel->SetSizer(Sizer);
-  Panel->Layout();
-  return Panel;
-};
+  SetSizer(Sizer);
+  Layout();
+}
+
+
+//clang-format off
+wxBEGIN_EVENT_TABLE(GeneralPanel, wxPanel)
+  EVT_CHOICE(ID_LANG, GeneralPanel::OnLang)
+  EVT_CHECKBOX(ID_LAUNCHER_UPDATES, GeneralPanel::OnLauncherUpdates)
+  EVT_CHECKBOX(ID_GAME_UPDATES, GeneralPanel::OnGameUpdates)
+  EVT_TEXT(ID_GAMES_DIR_EDIT, GeneralPanel::OnGameDir)
+wxEND_EVENT_TABLE()
+
+wxBEGIN_EVENT_TABLE(DownloadPanel, wxPanel)
+
+wxEND_EVENT_TABLE()
+
+wxBEGIN_EVENT_TABLE(RepositoryPanel, wxPanel)
+
+wxEND_EVENT_TABLE()
+//clang-format on
+
+
